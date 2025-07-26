ce8566c84a0a83151113dc951056be4b57f526ab
Infernario1130 <infernario11@gmail.com>
2025-07-26 17:34:53 +0530

    Add NotFound Page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,18 @@
+import Confused from "../assets/Confused.png";
+
+
 export default function NotFound() {
     return (
-        <>
-        <h1>404- Not Found</h1>
-        </>
+        <div className="min-h-screen bg-gradient-to-br from-rose-300 to-pink-500 flex justify-center items-center flex-col ">
+                
+                <div className="font-bold text-5xl animate-bounce"> 
+                    404-Not Found 
+                </div>
+
+
+                <div>
+                    <img src={Confused} alt="illustraion"></img>
+                </div>                
+        </div>
     )
 }
